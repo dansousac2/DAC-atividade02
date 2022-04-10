3c7e9f5876ef3db536bdb3ce028b963daac7481d
DanSousa <dansousac2@gmail.com>
2022-04-10 20:08:33 -0300

    funcionalidade delete de Guest ok

--- src/main/java/com/edu/ifpbdanilo_costa/atv02/atividade02/model/Controller/GuestController.java
+++ src/main/java/com/edu/ifpbdanilo_costa/atv02/atividade02/model/Controller/GuestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -65,12 +66,19 @@ public class GuestController {
 		}
 
 	}
-//
-//	public void delete(Long cpf) {
-//		if (validation.isValidCpf(cpf)) {
-//			guestService.delete(cpf);
-//		}
-//	}
+
+	@DeleteMapping("/delete")
+	public ResponseEntity delete(@RequestBody GuestDto dto) {
+		try {
+			validation.isValidCpf(dto.getCpf());
+			
+			guestService.delete(dto.getCpf());
+			
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
 //
 //	public void listAll() {
 //		guestService.showAll();
